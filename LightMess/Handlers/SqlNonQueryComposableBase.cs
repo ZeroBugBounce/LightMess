fbde93f4cec090e4267c068810bf332933ec50a7
ZeroBugBounce <richardlowe@gmail.com>
2012-08-30 02:30:06 -0500

    SqlNonQueryComposableBase

---
+++ LightMess/Handlers/SqlNonQueryComposableBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ZeroBugBounce.LightMess
+{
+	public abstract class SqlNonQueryComposableBase<TMessage> : Handler<TMessage> where TMessage : IConnectionMessage
+	{
+		public abstract SqlCommand PrepareCommand(TMessage message, CancellationToken cancellationToken);
+
+		public sealed override Task<Envelope> Handle(TMessage message, CancellationToken cancellationToken)
+		{
+			var taskComletionSource = new TaskCompletionSource<Envelope>();
+
+			Task.Factory.StartNew(() =>
+			{
+				try
+				{
+					Message.Post(new SqlNonQueryRequest(PrepareCommand(message, cancellationToken), message.ConnectionBuilder))
+						.Callback<SqlNonQueryResponse>((t, r) =>
+						{
+							try
+							{
+								taskComletionSource.SetResult(new Envelope<int>(r.AffectedRecords));
+							}
+							finally
+							{
+								r.Dispose();
+							}
+						});
+				}
+				catch (Exception ex)
+				{
+					taskComletionSource.SetException(ex);
+				}
+			});
+
+			return taskComletionSource.Task;
+		}
+	}
+}
